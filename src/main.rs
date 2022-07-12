79eb16b43e609a2c3bb8ed8ff66f68e1c0c46ea4
Aiden Lee <75740207+snaapsh0t12@users.noreply.github.com>
2022-07-12 10:06:37 -0500

    going through bevy tutorial

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,44 @@
+use bevy::prelude::*;
+
+#[derive(Component)]
+struct Position { x: f32, y: f32, z: f32 }
+
+#[derive(Component)]
+struct Person;
+
+#[derive(Component)]
+struct Name(String);
+
+
 fn main() {
-    println!("Hello, world!");
+    App::new()
+    .add_system(add_people)
+    .add_system(hello_world)
+    .add_system(greet_people)
+    .run();
 }
+
+fn print_position_system(query: Query<&Transform>) {
+    for transform in query.iter() {
+        println!("position: {:?}", transform.translation);
+    }
+}
+
+fn greet_people(query: Query<&Name, With<Person>>) {
+    for name in query.iter() {
+        println!("hello {}!", name.0);
+    }
+}
+
+fn add_people(mut commands: Commands) {
+    commands.spawn().insert(Person).insert(Name("Elaina Proctor".to_string()));
+    commands.spawn().insert(Person).insert(Name("Renzo Hume".to_string()));
+    commands.spawn().insert(Person).insert(Name("Zayna Nieves".to_string()));
+}
+
+fn hello_world() {
+    println!("hello world!");
+}
+
+
+struct Entity(u64);
